3a73596bc8913eeef2a55090dac8060a52617b09
Aaron Watters <awatters@simonsfoundation.org>
2020-02-06 14:31:21 -0500

    mock canvas

--- jest/globals.js
+++ jest/globals.js
@@ -4,4 +4,27 @@
 if (!global.jQuery) {
     global.jQuery = require('jquery');
 }
-  
+
+
+function mockCanvas (window) {
+    var imgdata = function(w, h) {
+        var length = w * h * 4;
+        var result = new Array(length);
+        for (var i=0; i<length; i++) {
+            result[i] = 0;
+        }
+        return result;
+    }
+    window.HTMLCanvasElement.prototype.getContext = function (kind) {
+        return {
+            _info: "This is a mock canvas context",
+            _kind: kind
+        };
+    }
+
+    window.HTMLCanvasElement.prototype.toDataURL = function () {
+        return "";
+    }
+}
+
+global.mockCanvas = mockCanvas;
