e6986b60bd7ce7e22e4fb64827d32afdc077ae4f
Aaron Watters <awatters@simonsfoundation.org>
2020-07-13 11:36:14 -0400

    add local option

--- feedWebGL2/local_files.py
+++ feedWebGL2/local_files.py
@@ -8,5 +8,5 @@ import os
 
 my_dir = os.path.dirname(__file__)
 
-def vendor_path(filename):
-    return js_context.get_file_path(filename, my_dir=my_dir)
+def vendor_path(filename, local=False):
+    return js_context.get_file_path(filename, my_dir=my_dir, local=local)
