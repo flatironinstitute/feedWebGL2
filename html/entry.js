380847260d28f26c7e175fc92951e3887559e0f9
Aaron Watters <awatters@simonsfoundation.org>
2021-05-04 06:56:24 -0400

    support marching cubes and volume32 in html tests

--- html/entry.js
+++ html/entry.js
@@ -7,15 +7,35 @@ require("../dist/index");
 
 var runr, d;  // keep context
 
+/*
 jQuery(function(){
   debugger;
   var $ = jQuery;
   var d = $("#container");
   //var runr = jQuery.fn.feedWebGL2.trivial_example(d);
   //var runr = jQuery.fn.feedWebGL2.example(d);
-  var contour = jQuery.fn.webGL2surfaces3d.simple_example(d);
+  //var contour = jQuery.fn.webGL2surfaces3d.simple_example(d);
+  //var test = $.fn.streamLiner.example(d);
+  //var test = $.fn.webGL2MarchingCubes.example(d);
+  var test = $.fn.volume32.example(d);
 });
+*/
 
+function main_test_program() {
+
+    debugger;
+    var $ = jQuery;
+    d = $("#container");
+    //var runr = jQuery.fn.feedWebGL2.trivial_example(d);
+    //var runr = jQuery.fn.feedWebGL2.example(d);
+    //var contour = jQuery.fn.webGL2surfaces3d.simple_example(d);
+    //var test = $.fn.streamLiner.example(d);
+    //var test = $.fn.webGL2MarchingCubes.example(d);
+    var test = $.fn.volume32.example(d);
+
+}
+
+window.main_test_program = main_test_program;
 /*
 helpful console snippet:
 
